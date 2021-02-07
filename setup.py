6e48e2153c6158e9f8cadb2f679b9a4d1f51e582
Gustavo Eguez <eguezgustavo@gmail.com>
2021-02-07 17:21:53 -0500

    parent folder

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+  name='cmxtelegrambot',
+  version='0.0.1',
+  description='A telegram bot that acts as a form',
+  url='git@github.com:eguezgustavo/cmx_telegram_bot.git',
+  author='Gustavo Eguez',
+  author_email='eguezgustavo@gmail.com',
+  license='LICENSE.txt',
+  packages=['cmxtelegrambot'],
+)
